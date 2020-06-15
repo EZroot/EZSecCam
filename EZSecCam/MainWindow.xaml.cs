8d6336c9db393eb20df23a9b8afa71953f3a5b54
EZRoot <utohaha@gmail.com>
2020-06-15 01:26:28 -0230

    added: haarcascade to live webcam, settings file

--- EZSecCam/MainWindow.xaml.cs
+++ EZSecCam/MainWindow.xaml.cs
@@ -35,7 +35,7 @@ namespace EZSecCam
             
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void StartCameraButton_Click(object sender, RoutedEventArgs e)
         {
             StartWebcam();
         }
@@ -56,32 +56,73 @@ namespace EZSecCam
 
             try
             {
-                capture.Open(1);
+                capture.Open(0);
 
                 Console.WriteLine("GetBackendName " + capture.GetBackendName());
                 Console.WriteLine("Channel " + capture.Get(VideoCaptureProperties.Channel));
                 Console.WriteLine("Guid " + capture.Guid);
                 Console.WriteLine("IsOpened " + capture.IsOpened());
-                Console.WriteLine("capture " + capture.ToString());
-
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.Write("Failed to find cam: "+e.Message);
+                return;
             }
 
+            //TODO: Create thread to make webcam faster
             DispatcherTimer Timer = new DispatcherTimer();
-            Timer.Tick += Timer_Tick;
-            Timer.Interval = TimeSpan.FromMilliseconds(30);
+            Timer.Tick += CameraFrameUpdate;
+            Timer.Interval = TimeSpan.FromMilliseconds(230);
             Timer.Start();
+
+            //TODO: Create thread to make face detection faster
+            DispatcherTimer Timer2 = new DispatcherTimer();
+            Timer2.Tick += CascadeFaceDetectorFrameUpdate;
+            Timer2.Interval = TimeSpan.FromMilliseconds(30);
+            Timer2.Start();
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void CameraFrameUpdate(object sender, EventArgs e)
         {
             if (capture.Read(frame))
             {
-                Image1.Source = BitmapSourceConverter.ToBitmapSource(frame);
+                switch(Settings.filterType)
+                {
+                    case Settings.FilterType.None:
+                        break;
+                    case Settings.FilterType.FilterBrightness:
+                        Settings.UpdateBrightnessContrast(frame, frame, 50, 20);
+                        break;
+                }
+
+                WebcamImage.Source = BitmapSourceConverter.ToBitmapSource(frame);
             }
+        }
+
+        private void CascadeFaceDetectorFrameUpdate(object sender, EventArgs e)
+        {
+            if (capture.Read(frame))
+            {
+                var haarCascade = new CascadeClassifier(Settings.HarrcascadePath);
+                Mat haarResult = Settings.DetectFace(haarCascade, frame);
+
+                HistogramImage.Source = BitmapSourceConverter.ToBitmapSource(haarResult);
+            }
+        }
+
+        private void FilterCamButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.filterType = Settings.FilterType.FilterBrightness;
+        }
+
+        private void ResetFilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.filterType = Settings.FilterType.None;
+        }
+
+        private void HistogramButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ EZSecCam/Settings.cs
@@ -0,0 +1,79 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Media;
+using Point = OpenCvSharp.Point;
+
+namespace EZSecCam
+{
+    public class Settings
+    {
+        public enum FilterType
+        {
+            None,
+            FilterBrightness
+        }
+
+        public static FilterType filterType = FilterType.None;
+
+         public const string HarrcascadePath = "Data/haarcascade_frontalface_default.xml";
+         public const string FacepicturePath = "Data/yalta.jpg";
+        public const string FacepicturePathPng = "Data/lenna.png";
+
+        public static void UpdateBrightnessContrast(Mat src, Mat modifiedSrc, int brightness, int contrast)
+        {
+            brightness = brightness - 100;
+            contrast = contrast - 100;
+
+            double alpha, beta;
+            if (contrast > 0)
+            {
+                double delta = 127f * contrast / 100f;
+                alpha = 255f / (255f - delta * 2);
+                beta = alpha * (brightness - delta);
+            }
+            else
+            {
+                double delta = -128f * contrast / 100;
+                alpha = (256f - delta * 2) / 255f;
+                beta = alpha * brightness + delta;
+            }
+            src.ConvertTo(modifiedSrc, MatType.CV_8UC3, alpha, beta);
+        }
+
+        public static Mat DetectFace(CascadeClassifier cascade, Mat frame)
+        {
+            Mat result;
+
+            using (var gray = new Mat())
+            {
+                result = frame.Clone();
+                Cv2.CvtColor(frame, gray, ColorConversionCodes.BGR2GRAY);
+
+                // Detect faces
+                Rect[] faces = cascade.DetectMultiScale(
+                    gray, 1.08, 2, HaarDetectionType.ScaleImage, new OpenCvSharp.Size(30, 30));
+
+                // Render all detected faces
+                foreach (Rect face in faces)
+                {
+                    var center = new Point
+                    {
+                        X = (int)(face.X + face.Width * 0.5),
+                        Y = (int)(face.Y + face.Height * 0.5)
+                    };
+                    var axes = new OpenCvSharp.Size
+                    {
+                        Width = (int)(face.Width * 0.5),
+                        Height = (int)(face.Height * 0.5)
+                    };
+                    Cv2.Ellipse(result, center, axes, 0, 0, 360, new Scalar(255, 0, 255), 4);
+                }
+            }
+            return result;
+        }
+    }
+}
